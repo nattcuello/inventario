3f1520840623a376089aec141abbbde624ff2062
natalia <n.cuelloitecriocuarto.org.ar>
2025-06-20 19:00:26 -0300

    first commit

--- Escritorio/tercer_año/e-comerce/ecommers/products/apps.py
+++ Escritorio/tercer_año/e-comerce/ecommers/products/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EcommersConfig(AppConfig):
+class ProductsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ecommers'
+    name = 'products'
